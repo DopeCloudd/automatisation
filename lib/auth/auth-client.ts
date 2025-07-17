17ae950664d810a72904e8ae8464ac43fb833e16
Valentin Lerouge <cloudd2vi@gmail.com>
2025-07-17 22:45:04 +0200

    Mise à jour de l'URL de base pour le client d'authentification vers "https://zoom.automatisationdocuments.ovh"

--- lib/auth/auth-client.ts
+++ lib/auth/auth-client.ts
@@ -2,7 +2,8 @@ import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3001",
+  baseURL:
+    process.env.BETTER_AUTH_URL || "https://zoom.automatisationdocuments.ovh",
   plugins: [adminClient()],
 });
 
